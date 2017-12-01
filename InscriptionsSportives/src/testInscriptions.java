0a2f61c318fb97d04df642f697b96293e4b9d88d
jaune-attend <jo.truong01@gmail.com>
2017-12-01 17:18:37 +0100

    testUnit suite

--- InscriptionsSportives/src/testInscriptions.java
+++ InscriptionsSportives/src/testInscriptions.java
@@ -2,11 +2,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import inscriptions.Competition;
+import inscriptions.Equipe;
+import inscriptions.Inscriptions;
+import inscriptions.Personne;
+
 class testInscriptions {
 
+	Inscriptions inscriptions = Inscriptions.getInscriptions();
+	Competition flechettes = inscriptions.createCompetition("Mondial de fléchettes", null, false);
+	Personne tony = inscriptions.createPersonne("Tony", "Dent de plomb", "azerty"), 
+			boris = inscriptions.createPersonne("Boris", "le Hachoir", "ytreza");
+	Equipe lesManouches = inscriptions.createEquipe("Les Manouches");
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void EquipeSetnom() {
+		lesManouches.setNom("Vince");
+		assertEquals("Vince", lesManouches.getNom());
+	}
+	
+	void CompetitionsSetnom()
+	{
+		flechettes.setNom("Cauchemard");
+		assertEquals("Cauchemard", flechettes.getNom());
 	}
 
+	void PersonneSetnom()
+	{
+		tony.setNom("Gerard");
+		assertEquals("Gerard", tony.getNom());
+	}
+	
+	void PersonneSetmail()
+	{
+		tony.setMail("test@gmail.com");
+		assertEquals("test@gmail.com", tony.getMail());
+	}
+	
+	
 }
