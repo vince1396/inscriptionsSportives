82734fe3fa6a923b55f7131b30b9219f25eaf865
jaune-attend <jo.truong01@gmail.com>
2018-03-09 09:53:32 +0100

    update

--- InscriptionsSportives/src/inscriptions/Competition.java
+++ InscriptionsSportives/src/inscriptions/Competition.java
@@ -58,7 +58,7 @@ public class Competition implements Comparable<Competition>, Serializable
 	public boolean inscriptionsOuvertes()
 	{
 		// TODO retourner vrai si et seulement si la date système est antérieure à la date de clôture.
-		return true;
+		return this.dateCloture.isAfter(LocalDate.now());
 	}
 	
 	/**
